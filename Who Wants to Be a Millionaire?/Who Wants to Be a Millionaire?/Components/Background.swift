0a436ad8751b6ecc047fa410202df24b24406ccd
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-26 01:10:47 +0300

    added background view

---
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/Components/Background.swift
@@ -0,0 +1,18 @@
+//
+//  Background.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Artem on 26.02.2024.
+//
+
+import SwiftUI
+
+struct Background: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Background()
+}
---
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/Resources/Images.swift
@@ -0,0 +1,8 @@
+//
+//  Images.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Artem on 26.02.2024.
+//
+
+import Foundation
