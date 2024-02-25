ed33d3d5180b0fec355a5d5a1be2a6fa071eec8c
Ga1oker <54763540+Galoker1@users.noreply.github.com>
2024-02-25 17:21:56 +0300

    Добавлены файлы проекта

---
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Егор  Хлямов on 25.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/Who_Wants_to_Be_a_Millionaire_App.swift
@@ -0,0 +1,17 @@
+//
+//  Who_Wants_to_Be_a_Millionaire_App.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Егор  Хлямов on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Who_Wants_to_Be_a_Millionaire_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
