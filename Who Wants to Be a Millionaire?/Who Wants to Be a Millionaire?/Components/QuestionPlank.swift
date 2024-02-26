a137afc73d99d9452a0f5fd5370a8aa9dd1c70cf
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-26 14:18:52 +0300

    added gradients and plank component

---
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/Components/QuestionPlank.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionPlank.swift
+//  Who Wants to Be a Millionaire?
+//
+//  Created by Artem on 26.02.2024.
+//
+
+import SwiftUI
+
+struct QuestionPlank: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionPlank()
+}
