7d67f6b02e17891caa288b1cb2c3e0d390cbec16
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-26 15:44:12 +0300

    added endgame view

---
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/EndGameView.swift
@@ -0,0 +1,67 @@
+//
+//  EndGameView.swift
+//
+
+import SwiftUI
+
+struct EndGameView: View {
+    
+    @State var opacity: Double = 0
+    
+    var body: some View {
+        ZStack {
+            Background(image: BgImage.empty)
+            
+            LightAnimation(color: .red)
+            
+            Text("LOSE")
+                .font(.largeTitle)
+                .foregroundStyle(.red)
+                .opacity(opacity)
+                .animation(.easeInOut(duration: 1).delay(3), value: opacity)
+
+            
+            VStack {
+                Image("Logo")
+                    .resizableToFit()
+                    .frame(width: UIScreen.main.bounds.width*0.6)
+                
+                Text("You losed on 3 question")
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                    .padding(.top)
+                
+                Spacer()
+                
+                PlayAgainBtn(action: {})
+                
+            }
+            .opacity(opacity)
+            .animation(.easeInOut(duration: 1), value: opacity)
+        }
+        .onAppear {
+            opacity = 1
+            SoundService.player.play(key: .lose)
+        }
+    }
+    
+    func PlayAgainBtn(action: @escaping () -> Void) -> some View {
+        Button {
+            action()
+        } label: {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.green)
+                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .padding(80)
+                .overlay {
+                    Text("PLAY AGAIN")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle)
+                }
+        }
+    }
+}
+
+#Preview {
+    EndGameView()
+}
