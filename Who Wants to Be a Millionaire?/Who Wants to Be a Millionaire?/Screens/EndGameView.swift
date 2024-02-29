60ae3368d3df97c1ff9d442cd7dfcab8c705323e
Ga1oker <54763540+Galoker1@users.noreply.github.com>
2024-03-01 00:35:25 +0300

    Добавлена навигация и сохранение

--- Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/Screens/EndGameView.swift
+++ Who Wants to Be a Millionaire?/Who Wants to Be a Millionaire?/Screens/EndGameView.swift
@@ -5,53 +5,76 @@
 import SwiftUI
 
 struct EndGameView: View {
-    
+    @Environment(\.dismiss) var dismiss
     let questionIndex: Int
     let win: Bool
     let prize: Int
     let newGameClosure: () -> Void
-    
+    let name: String
     @State var opacity: Double = 0
     
     var body: some View {
-        ZStack {
-            Background(image: BgImage.empty)
-            
-            LightAnimation(color: .red)
-            
-            Text(win ? "WIN" : "LOSE")
-                .font(.largeTitle)
-                .foregroundStyle(.red)
-                .opacity(opacity)
-                .animation(.easeInOut(duration: 1).delay(3), value: opacity)
-
-            
-            VStack {
-                Image(.logo)
-                    .resizableToFit()
-                    .frame(width: UIScreen.main.bounds.width * 0.6)
+        NavigationView {
+            ZStack {
+                Background(image: BgImage.empty)
                 
-                Group {
-                    if win {
-                        Text("You won 1 million!")
-                            .foregroundStyle(.yellow)
-                    } else {
-                        Text("You losed on \(questionIndex + 1) question\nSafe Haven: \(prize) RUB")
+                LightAnimation(color: .red)
+                
+                Text(win ? "WIN" : "LOSE")
+                    .font(.largeTitle)
+                    .foregroundStyle(.red)
+                    .opacity(opacity)
+                    .animation(.easeInOut(duration: 1).delay(3), value: opacity)
+                
+                
+                VStack {
+                    Image(.logo)
+                        .resizableToFit()
+                        .frame(width: UIScreen.main.bounds.width * 0.6)
+                    
+                    Group {
+                        if win {
+                            Text("You won 1 million!")
+                                .foregroundStyle(.yellow)
+                        } else {
+                            Text("You losed on \(questionIndex + 1) question\nSafe Haven: \(prize) RUB")
+                        }
+                    }
+                    .foregroundStyle(.white)
+                    .padding(.top)
+                    .font(.title2)
+                    Spacer()
+                    
+                    playAgainBtn()
+                    
+                  
+                    NavigationLink(destination: MenuView()) {
+                        Text("MENU")
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.green)
+                                    .frame(height: 50)
+                                    .frame(width: 200)
+                            )
+                            .padding(.top, 30)
                     }
                 }
-                .foregroundStyle(.white)
-                .padding(.top)
-                .font(.title2)
-                Spacer()
-                
-                playAgainBtn()
-                
+                .opacity(opacity)
+                .animation(.easeInOut(duration: 1), value: opacity)
             }
-            .opacity(opacity)
-            .animation(.easeInOut(duration: 1), value: opacity)
-        }
-        .onAppear {
-            opacity = 1
+            .onAppear {
+                opacity = 1
+                var results: Results? = UserDefaultService.shared.getStructs(forKey: "Result")
+                if results == nil {
+                    results = Results(resulsArray: [ResultsModel(id: UUID(), name: name, score: prize)])
+                } else {
+                    results?.resulsArray.append(ResultsModel(id: UUID(), name: name, score: prize))
+                }
+                UserDefaultService.shared.saveStructs(structs: results, forKey: "Result")
+            }
+            
         }
     }
     
@@ -59,15 +82,16 @@ struct EndGameView: View {
         Button {
             newGameClosure()
         } label: {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.green)
-                .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-                .padding(80)
-                .overlay {
-                    Text("PLAY AGAIN")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                }
+            Text("PLAY AGAIN")
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.green)
+                        .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        .frame(width: 300)
+                )
+  
         }
     }
 }
@@ -77,6 +101,7 @@ struct EndGameView: View {
         questionIndex: 10,
         win: false,
         prize: 1000,
-        newGameClosure: {}
+        newGameClosure: {}, 
+        name: "Egor"
     )
 }
